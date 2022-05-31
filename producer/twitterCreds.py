edb40a062cb29bbb2afbdaa7bb8c3eb181155d2a
Sean Coyne <scoyne2@kent.edu>
2022-05-31 03:37:40 +0000

    Setup Codespaces

--- producer/twitterCreds.py
+++ producer/twitterCreds.py
@@ -1,4 +1,6 @@
-consumer_key = "XXX"
-consumer_secret = "XXX"
-access_token_key = "XXX-XXX"
-access_token_secret = "XXX"
+import os 
+
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token_key = os.environ["ACCESS_TOKEN_KEY"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
